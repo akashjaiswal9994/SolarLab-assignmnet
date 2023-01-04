97c65d596e2c83b83ccf0c462a17f554851943bc
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2023-01-04 09:36:01 +0530

    scraping

--- assignment/scrap.py
+++ assignment/scrap.py
@@ -1,11 +1,32 @@
-import request
+import requests
 from bs4 import BeautifulSoup
 import json
 
 
-res=request.get("https://en.wikipedia.org/wiki/India")
+res=requests.get("https://en.wikipedia.org/wiki/India")
 
 soup = BeautifulSoup(res.content,'html.parser')
-print(soup.prettify())
+#python assignment/scrap.pyprint(soup.prettify())
 
 
+def get_infobox(url):
+       response = requests.get(url)
+       bs = BeautifulSoup(response.content,'html.parser')
+
+       table = bs.find('table', {'class' :'infobox ib-country vcard'})
+       result = {}
+       row_count = 0
+       if table is None:
+         pass
+       else:
+         for tr in table.find_all('tr'):
+             if tr.find('th'):
+                 pass
+             else:
+                 row_count += 1
+         if row_count > 1:
+             if tr is not None:
+               result[tr.find('td').text.strip()] = tr.find('td').text
+         return result
+
+print(get_infobox("https://en.wikipedia.org/wiki/India"))        
